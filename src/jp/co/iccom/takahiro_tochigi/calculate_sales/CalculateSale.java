3fd601a5a59437d614a581e3d37d99e511267cae
Tochigi Takahiro <t_tochigi@ic-com.cojp>
2016-04-26 16:47:59 +0900

    売上課題

--- src/jp/co/iccom/takahiro_tochigi/calculate_sales/CalculateSale.java
+++ src/jp/co/iccom/takahiro_tochigi/calculate_sales/CalculateSale.java
@@ -1,35 +1,48 @@
 package jp.co.iccom.takahiro_tochigi.calculate_sales;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class CalculateSale {
+	
 	public static void main(String[] args){
-		for(int i = 0 ;i< args.length; i++){
-			System.out.println(args[i]);//fileをコマンドライン引数で開く
-		}
-		try{
-			String line;//保持する
-			FileReader file = new FileReader(args[0]);
-			BufferedReader br = new BufferedReader(file);
+	HashMap<String,String> branchlist =new  HashMap<String,String>();//データを保持するmap作成
+	HashMap<String,String> commoditylist = new  HashMap<String,String>();//データを保持するmap作成
+	 String stBufferBR = ""; //戻り値を格納 
+     String stBufferCO = ""; //戻り値を格納 
+	
 
-			while ((line = br.readLine()) != null){
-				String[] lines = line.split(",");
-				
-				if(){
-					
-				}
-				
-				for(String bl: lines){
-					System.out.println(bl);//確認のため分割した配列の表示
+		try{
+			File  filea =new  File("C:\\UriageFile\\branch.lst");
+			FileReader fra = new FileReader(filea);
+			BufferedReader bra = new BufferedReader(fra);//fileの入力
+			//System.out.println(br);
+			
+			File  fileb =new  File("C:\\UriageFile\\commodity.lst");
+			FileReader frb = new FileReader(fileb);
+			BufferedReader brb = new BufferedReader(frb);//fileの入力
+			
+			while ((stBufferBR = bra.readLine()) != null){
+				String[] namea = stBufferBR.split(",");//
+				branchlist.put(namea[0],namea[1]);
+			
+			while((stBufferCO = brb.readLine())  != null){
+				String[] nameb = stBufferCO.split(",");
+				commoditylist.put(nameb[0],nameb[1]);
+			};				
+					System.out.println(branchlist);//確認のため分割した配列の表示
+					System.out.println(commoditylist);
+					//後で略
 					}
-			}
-			br.close();
+			bra.close();
+			brb.close();
 		}catch(IOException e){
 			System.out.println("支店定義ファイルが存在しません");//ファイルの存在に関するエラー
 			System.out.println(e);
 		}
-		
 	}
 }
+	
