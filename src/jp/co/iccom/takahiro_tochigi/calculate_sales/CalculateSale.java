2159f3f6eed3f22ce5b8cc3b9f7baad6f78d0a26
Tochigi Takahiro <t_tochigi@ic-com.cojp>
2016-05-11 18:06:46 +0900

    売上集計」

--- src/jp/co/iccom/takahiro_tochigi/calculate_sales/CalculateSale.java
+++ src/jp/co/iccom/takahiro_tochigi/calculate_sales/CalculateSale.java
@@ -1,28 +1,35 @@
 package jp.co.iccom.takahiro_tochigi.calculate_sales;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
-
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 public class CalculateSale {
 
 	public static void main(String[] args){
 
 		HashMap<String,String> branchList =new  HashMap<String,String>();//データを保持するmap作成
 		HashMap<String,String> commodityList = new  HashMap<String,String>();//データを保持するmap作成
- 		ArrayList<File> allfile = new ArrayList<File>();//売上ファイルのリストを保持するためのリスト
- 		HashMap<String,Integer> store = new HashMap<>();//支店の売上合計を計算するため
- 		HashMap<String,Integer> product = new HashMap<>();//商品の売上合計を計算するため
- 		
+		ArrayList<File> allfile = new ArrayList<File>();//売上ファイルのリストを保持するためのリスト
+		HashMap<String,Integer> store = new HashMap<>();//支店の売上合計を保持する
+		HashMap<String,Integer> product = new HashMap<>();//商品の売上合計を保持する
+		
 		String stBufferBR = ""; //戻り値を格納
 		String stBufferCO = ""; //戻り値を格納
-
-
-//支店番号と支店名のハッシュマップ
+		
+		
+		//支店番号と支店名のハッシュマップ
 		try{
 			File  filea =new  File(args[0], "branch.lst");
 			FileReader fra = new FileReader(filea);
@@ -53,7 +60,7 @@ public class CalculateSale {
 				product.put(nameb[0], 0);
 			
 			};
-			//System.out.println(branchlist);//確認のため分割した配列の表示
+			//System.out.println(branchList);//確認のため分割した配列の表示
 			//System.out.println(commoditylist);
 			//後で略
 
@@ -78,21 +85,22 @@ public class CalculateSale {
  		for (int i = 0; i < allfile.size(); i++) {
  			String fileName = allfile.get(i).getName();
  			String number = fileName.split("\\.")[0];
- 			int inumber = Integer.parseInt(number);
+ 			int intNumber = Integer.parseInt(number);
  			//System.out.println(Integer.parseInt(allfile.get(i).getName().split("\\.")[0]));
- 			numberfile.add(inumber);
+ 			numberfile.add(intNumber);
  			//System.out.println(numberfile);
 
  			if(numberfile.size() == numberfile.get(numberfile.size() - 1)){
 
- 			}else{
- 			System.out.println("売上ファイルが連番になっていません。");
- 			//実行処理を出た時点で終了するように処理してください。
+			}else{
+			System.out.println("売上ファイルが連番になっていません。");
+			break;
+			//実行処理を出た時点で終了するように処理してください。
  			}
  		}
  		//allfileに入っているrcdファイルを繰り返し処理で開いていく
  		for(int i = 0; i<allfile.size(); i++){
- 			ArrayList<String> extraction = new ArrayList<>();
+ 			ArrayList<String> extraction = new ArrayList<>();//抽出した売上ファイルを保持するリスト
  			try {
 				FileReader fru =new FileReader(allfile.get(i));
 				BufferedReader brf = new BufferedReader(fru);
@@ -141,28 +149,76 @@ public class CalculateSale {
 				
 			}catch (IOException e ){
 			} 			
- 		}
+		}
+		//支店別集計ファイル　降順
+		List<Map.Entry<String,Integer>> entries = 
+		new ArrayList<Map.Entry<String,Integer>>(store.entrySet());
+		Collections.sort(entries, new Comparator<Map.Entry<String,Integer>>() {
+		public int compare(
+		Entry<String,Integer> entry1, Entry<String,Integer> entry2) {
+		return ((Integer)entry2.getValue()).compareTo((Integer)entry1.getValue());
+		}
+		}); 
+		/*for (Entry<String,Integer> s : entries) {
+			System.out.println("s.getKey() : " + s.getKey());
+			System.out.println("s.getValue() : " + s.getValue());
+		}*/
+		//System.out.println(entries);
+		//
 		
+		//商品別集計ファイル　降順
+		List<Map.Entry<String,Integer>> commoditydown = 
+		new ArrayList<Map.Entry<String,Integer>>(product.entrySet());
+		Collections.sort( commoditydown, new Comparator<Map.Entry<String,Integer>>() {
+		public int compare(
+		Entry<String,Integer> entry1, Entry<String,Integer> entry2) {
+		return ((Integer)entry2.getValue()).compareTo((Integer)entry1.getValue());
+		}
+		}); 		
+		//支店別売上集計ファイルに出力するためのレイアウト
+		/* for(String branchKey :branchList.keySet()){
+			 System.out.println( branchKey + "," 
+		 + branchList.get(branchKey)+","+ store.get(branchKey));
+         }
+		 //商品別売上集計ファイルに出力するためのレイアウト
+		 for(String commodityKey :commodityList.keySet()){
+             System.out.println( commodityKey + "," 
+		 + commodityList.get(commodityKey)+","+ product.get(commodityKey));
+         }*/
 		//System.out.println(product);
 		
-		//コマンドライン引数で指定されたディレクトリに支店別集計ファイルを作成する。
-		/*try{
-			File directry1 = new File("branch.out");
-			if(directry1.mkdir()){
-				System.out.println(directry1 +"を作成");
-			}
-
-			File file =new File	(args[0], "branch.out");
-			FileWriter fw = new FileWriter(file);
-			BufferedWriter bw =new BufferedWriter(fw);
+		//作成したファイルに集計ファイルを合計金額の降順で出力
+ 		
+ 		
+		try {
+			File branchOutFile = new File(args[0], "branch.out");
+			FileWriter fileWriter = new FileWriter(branchOutFile);
+			BufferedWriter bw = new BufferedWriter(fileWriter);
 			PrintWriter pw = new PrintWriter(bw);
-			
-			
-			
-		}catch(IOException){
-			
-		}*/
+			//
+			for (Entry<String,Integer> s : entries) {
+				pw.println( s.getKey() + "," 
+			+ branchList.get( s.getKey() )+","+ store.get( s.getKey() ) );
+			}
+			pw.close();
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		try {
+			File branchOutFile = new File(args[0], "commodity.out");
+			FileWriter fileWriter = new FileWriter(branchOutFile);
+			BufferedWriter bw = new BufferedWriter(fileWriter);
+			PrintWriter pw = new PrintWriter(bw);
+			//
+			for (Entry<String,Integer> s : commoditydown) {
+				pw.println( s.getKey() + "," 
+			+ commodityList.get( s.getKey() )+","+ product.get( s.getKey() ) );
+			}
+			pw.close();
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
  	}
 }
-
-
