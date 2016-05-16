05c29573bd727a99efa36169fd367e7d81013c7a
Tochigi Takahiro <t_tochigi@ic-com.cojp>
2016-05-16 18:24:25 +0900

    test

--- caluculate_sales/caluclate_sales/src/jp/co/iccom/tochigi_takahiro/calculate_sales/CalculateSales.java
+++ caluculate_sales/caluclate_sales/src/jp/co/iccom/tochigi_takahiro/calculate_sales/CalculateSales.java
@@ -150,7 +150,6 @@ public class CalculateSales{
 						System.out.println(files[i].getName()+"の商品コードが不正です");
 						return;
 					}
-
 					// 集計処理
 					// 足したい値を取得
 					long extraprice = Long.parseLong( extraction.get(2) );
